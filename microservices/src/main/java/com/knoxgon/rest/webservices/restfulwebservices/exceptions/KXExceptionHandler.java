35ebf0819e0b0b273c00742e1871feb2b6af13a4
knoxgon <volkan.guven@hotmail.se>
2018-11-19 16:18:46 +0100

    More exceptions added to custom exception handler

--- microservices/src/main/java/com/knoxgon/rest/webservices/restfulwebservices/exceptions/KXExceptionHandler.java
+++ microservices/src/main/java/com/knoxgon/rest/webservices/restfulwebservices/exceptions/KXExceptionHandler.java
@@ -16,7 +16,17 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class KXExceptionHandler extends ResponseEntityExceptionHandler{
 	
 	@ExceptionHandler(AccessDeniedException.class)
-	public final ResponseEntity<Object> handleGeneralExceptions(Exception ex, WebRequest req){
+	public final ResponseEntity<Object> handleAccessExceptions(Exception ex, WebRequest req){
 		return new ResponseEntity<Object>("Only authorized users are allowed", new HttpHeaders(), HttpStatus.FORBIDDEN);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleGeneralException(Exception ex, WebRequest req){
+		return new ResponseEntity<Object>("Internal Error", new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleNotFoundException(Exception ex, WebRequest req){
+		return new ResponseEntity<Object>("User not found", new HttpHeaders(), HttpStatus.NOT_FOUND);
 	}
 }
