749009a376ac3d4ba0e3d654d92acddc07513b93
knoxgon <volkan.guven@hotmail.se>
2018-11-19 17:04:57 +0100

    Added method to delete user

--- microservices/src/main/java/com/knoxgon/rest/webservices/restfulwebservices/userpcg/UserDaoService.java
+++ microservices/src/main/java/com/knoxgon/rest/webservices/restfulwebservices/userpcg/UserDaoService.java
@@ -9,27 +9,31 @@ import org.springframework.stereotype.Component;
 //Following service must be managed by spring, @Component is therefore required
 @Component
 public class UserDaoService {
-	private static List<User> users = new ArrayList<>();
+	private List<UserBean> users = new ArrayList<>();
 
 	// Method to save the user to DB
-	public User saveUser(User user) {
+	public UserBean saveUser(UserBean user) {
 		user.setId(UUID.randomUUID().toString());
-		users.add(user);
+		this.users.add(user);
 		return user;
 	}
 
 	// Method to find and retrieve all users
-	public List<User> allUsers() {
-		return users;
+	public List<UserBean> allUsers() {
+		return this.users;
 	}
 
-	// Method to find one user
-	public User getOneUser(String id) {
-		for(User user: users) {
+	// Method to find a user
+	public UserBean getOneUser(String id) {
+		for(UserBean user: this.users) {
 			if(user.getId().equals(id))
 				return user;
 		}
 		return null;
 	}
 	
+	//Method to delete a user
+	public UserBean deleteUserByID(String id) {
+		this.users.forEach(action);
+	}
 }
